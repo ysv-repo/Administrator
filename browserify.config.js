8b2c58d898f055c2600e09ee3066baca9db76dc9
vi-ra <89659874+vi-ra@users.noreply.github.com>
2023-05-18 21:39:17 +0530

    Initial Commit
    By Vinod

---
+++ browserify.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  standalone: 'XlsxPopulate',
+  browserField: false,
+  builtins: false,
+  assert: false,
+  os: false,
+  path: false,
+  process: false,
+  stream: false,
+  util: false,
+  vm: false,
+  fs: false,
+  constants: false
+};
